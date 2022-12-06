a889f7bc428d1b2a6ad470a64efad214e8f59330
red0ahlam <109111970+red0ahlam@users.noreply.github.com>
2022-12-06 01:17:14 +0100

    Update sunnyside-agency-landing-page-main1.js

--- sunnyside-agency-landing-page-main1.js
+++ sunnyside-agency-landing-page-main1.js
@@ -2,16 +2,24 @@
 const menu_button = document.getElementById("menu-button");
 const nav_items = document.getElementById("nav-items");
 const arrow_icon = document.getElementById("arrow-icon");
+const main_navigation = document.querySelector(".main-navigation");
 let x = true;
 
 
-menu_button.addEventListener('click', () => {
+function display () {
     nav_items.classList.toggle("display");
     arrow_icon.classList.toggle("icon-display");
     menu_button.setAttribute("aria-expanded",x);
-    if(x) {
-        x = false;
-    }else {
-        x = true;
+    (x) ? x = false : x = true ;
+}
+
+document.onclick = function (e) {
+    
+    if(e.target.matches("#menu-button")) {
+        display();
     }
-})
+    else if( (x == false) && !(e.target.matches(".main-navigation"))) {
+        display();
+    }
+    
+}
