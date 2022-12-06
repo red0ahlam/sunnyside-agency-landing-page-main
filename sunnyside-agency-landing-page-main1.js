f908d6e4db7d54ac24fa1e50534f89aef78ba086
red0ahlam <109111970+red0ahlam@users.noreply.github.com>
2022-12-06 17:43:23 +0100

    Update sunnyside-agency-landing-page-main1.js

--- sunnyside-agency-landing-page-main1.js
+++ sunnyside-agency-landing-page-main1.js
@@ -14,12 +14,14 @@ function display () {
 }
 
 document.onclick = function (e) {
-    
+
+    let nav_list = e.target.parentNode;
+    let nav_child = e.target;
+
     if(e.target.matches("#menu-button")) {
         display();
     }
-    else if( (x == false) && !(e.target.matches(".main-navigation"))) {
+    else if( (x == false) && !(nav_child.classList.contains("nav-items")) && !(nav_list.classList.contains("nav-items"))) {
         display();
     }
-    
 }
